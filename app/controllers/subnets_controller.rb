8e5263c176363d04c3802e06da0ca40b200466d1
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-17 11:58:14 -0400

    t

--- app/controllers/subnets_controller.rb
+++ app/controllers/subnets_controller.rb
@@ -1,52 +1,57 @@
-# app/controllers/subnets_controller.rb
 class SubnetsController < ApplicationController
   before_action :set_subnet, only: [:show, :edit, :update, :destroy]
 
+  # GET /subnets
   def index
     @subnets = Subnet.all
   end
 
-  def show
-  end
-
+  # GET /subnets/new
   def new
     @subnet = Subnet.new
-    @vm_networks = VmNetwork.all # Assuming you have a VmNetwork model
+    @vm_networks = fetch_vm_networks
   end
 
+  # GET /subnets/1/edit
+  def edit
+    @vm_networks = fetch_vm_networks
+  end
+
+  # POST /subnets
   def create
     @subnet = Subnet.new(subnet_params)
     if @subnet.save
-      redirect_to @subnet, notice: 'Subnet was successfully created.'
+      redirect_to subnets_path, notice: 'Subnet was successfully created.'
     else
+      @vm_networks = fetch_vm_networks
       render :new
     end
   end
 
-  def edit
-    @vm_networks = VmNetwork.all # Assuming you have a VmNetwork model
-  end
-
+  # PATCH/PUT /subnets/1
   def update
     if @subnet.update(subnet_params)
-      redirect_to @subnet, notice: 'Subnet was successfully updated.'
+      redirect_to subnets_path, notice: 'Subnet was successfully updated.'
     else
+      @vm_networks = fetch_vm_networks
       render :edit
     end
   end
 
-  def destroy
-    @subnet.destroy
-    redirect_to subnets_url, notice: 'Subnet was successfully destroyed.'
-  end
-
   private
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_subnet
     @subnet = Subnet.find(params[:id])
   end
 
+  # Only allow a list of trusted parameters through.
   def subnet_params
     params.require(:subnet).permit(:name, :address, :cidr, :mask, :gateway, :dns1, :dns2, :vm_network_id)
+  end
+
+  def fetch_vm_networks
+    # Adjust this method to fetch VM networks as needed
+    Vcenter.all.map { |vc| vc.networks }.flatten
   end
 end
