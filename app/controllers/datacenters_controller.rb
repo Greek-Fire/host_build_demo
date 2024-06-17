f22e3cccbbba6cbb8a10b220eb17bc5e65cfb026
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-17 11:51:38 -0400

    t

--- app/controllers/datacenters_controller.rb
+++ app/controllers/datacenters_controller.rb
@@ -1,9 +1,10 @@
+# app/controllers/datacenters_controller.rb
 class DatacentersController < ApplicationController
   before_action :set_vcenter
 
   def show
-    @datacenter = params[:id]
-    @compute_clusters = fetch_compute_clusters(@vcenter, @datacenter)
+    @vcenter_credentials = @vcenter.vcenter_credentials
+    @compute_clusters = fetch_compute_clusters(params[:id])
   end
 
   private
@@ -12,12 +13,12 @@ class DatacentersController < ApplicationController
     @vcenter = Vcenter.find(params[:vcenter_id])
   end
 
-  def fetch_compute_clusters(vcenter, datacenter_name)
+  def fetch_compute_clusters(datacenter_id)
     require 'fog/vsphere'
 
-    credential = vcenter.vcenter_credentials.first
+    credential = @vcenter.vcenter_credentials.first
     if credential.nil?
-      Rails.logger.debug("No credentials found for vCenter #{vcenter.id}")
+      Rails.logger.debug("No credentials found for vCenter #{@vcenter.id}")
       return []
     end
 
@@ -25,33 +26,16 @@ class DatacentersController < ApplicationController
       provider: 'Vsphere',
       vsphere_username: credential.username,
       vsphere_password: credential.password,
-      vsphere_server: vcenter.url,
+      vsphere_server: @vcenter.url,
       vsphere_ssl: credential.ssl_verification,
       vsphere_expected_pubkey_hash: 'a7401d408f9a4ac60848fe34242a9a9c89fcfb73231b2053f64540e8fb03721e',
       vsphere_insecure: !credential.ssl_verification
     )
 
-    datacenter = connection.datacenters.get(datacenter_name)
-    datacenter.clusters.map do |cluster|
-      {
-        name: cluster.name,
-        num_hosts: cluster.num_host,
-        total_storage: calculate_total_storage(cluster),
-        total_vm_networks: calculate_total_vm_networks(cluster),
-        availability: cluster.overall_status
-      }
-    end
+    datacenter = connection.datacenters.get(datacenter_id)
+    datacenter.clusters
   rescue => e
     Rails.logger.error("Failed to fetch compute clusters: #{e.message}")
     []
   end
-
-  def calculate_total_storage(cluster)
-    cluster.datastores.map(&:capacity).sum
-  end
-
-  def calculate_total_vm_networks(cluster)
-    cluster.networks.size
-  end
 end
-
--- app/controllers/subnets_controller.rb
+++ app/controllers/subnets_controller.rb
@@ -1,43 +1,43 @@
 # app/controllers/subnets_controller.rb
-
 class SubnetsController < ApplicationController
-  before_action :set_subnet, only: [:edit, :update, :destroy]
+  before_action :set_subnet, only: [:show, :edit, :update, :destroy]
 
   def index
     @subnets = Subnet.all
   end
 
+  def show
+  end
+
   def new
     @subnet = Subnet.new
-    @vm_networks = VmNetwork.all
+    @vm_networks = VmNetwork.all # Assuming you have a VmNetwork model
   end
 
   def create
     @subnet = Subnet.new(subnet_params)
     if @subnet.save
-      redirect_to subnets_path, notice: 'Subnet was successfully created.'
+      redirect_to @subnet, notice: 'Subnet was successfully created.'
     else
-      @vm_networks = VmNetwork.all
       render :new
     end
   end
 
   def edit
-    @vm_networks = VmNetwork.all
+    @vm_networks = VmNetwork.all # Assuming you have a VmNetwork model
   end
 
   def update
     if @subnet.update(subnet_params)
-      redirect_to subnets_path, notice: 'Subnet was successfully updated.'
+      redirect_to @subnet, notice: 'Subnet was successfully updated.'
     else
-      @vm_networks = VmNetwork.all
       render :edit
     end
   end
 
   def destroy
     @subnet.destroy
-    redirect_to subnets_path, notice: 'Subnet was successfully deleted.'
+    redirect_to subnets_url, notice: 'Subnet was successfully destroyed.'
   end
 
   private
@@ -46,4 +46,7 @@ class SubnetsController < ApplicationController
     @subnet = Subnet.find(params[:id])
   end
 
-  def
+  def subnet_params
+    params.require(:subnet).permit(:name, :address, :cidr, :mask, :gateway, :dns1, :dns2, :vm_network_id)
+  end
+end
