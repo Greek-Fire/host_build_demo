936cb9e94968c284a8e7484f5f0e7049b14b12a9
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-17 13:37:12 -0400

    t

--- db/migrate/20240606161408_add_vcenter_id_to_vcenter_credentials.rb
+++
@@ -1,7 +0,0 @@
-class AddVcenterIdToVcenterCredentials < ActiveRecord::Migration[6.0]
-  def change
-    add_column :vcenter_credentials, :vcenter_id, :integer
-    add_index :vcenter_credentials, :vcenter_id
-  end
-end
-
