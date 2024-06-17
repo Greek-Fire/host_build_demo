425906575ffbf2d0d94dedd08b3b962c82c2755f
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-17 11:06:03 -0400

    first

--- app/controllers/subnets_controller.rb
+++ app/controllers/subnets_controller.rb
@@ -1,2 +1,36 @@
 class SubnetsController < ApplicationController
-end
+    before_action :set_vm_networks, only: [:new, :create]
+  
+    def new
+      @subnet = Subnet.new
+    end
+  
+    def create
+      @subnet = Subnet.new(subnet_params)
+      if @subnet.save
+        redirect_to subnets_path, notice: 'Subnet was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def index
+      @subnets = Subnet.all
+    end
+  
+    private
+  
+    def subnet_params
+      params.require(:subnet).permit(:name, :address, :cidr, :mask, :gateway, :dns1, :dns2, :vm_network)
+    end
+  
+    def set_vm_networks
+      @vm_networks = fetch_vm_networks
+    end
+  
+    def fetch_vm_networks
+      # Assuming you have a method to fetch VM networks from vCenter
+      Vcenter.all.map(&:vm_networks).flatten
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,16 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :vcenters do
-    member do
-      post 'update_datacenters'
-    end
-    resources :datacenters, only: [:show] do
-      resources :compute_clusters, only: [:show] do
-        member do
-          get 'datastores'
-          get 'networks'
-        end
-      end
-    end
+    resources :datacenters, only: [:show]
     resources :vcenter_credentials, only: [:new, :create, :edit, :update, :destroy, :index, :show] do
       member do
         post 'verify'
       end
     end
+    member do
+      post 'update_datacenters'
+    end
   end
-end
 
+  resources :subnets, only: [:new, :create, :index]
+end
