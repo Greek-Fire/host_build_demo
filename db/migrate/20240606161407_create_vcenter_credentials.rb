1c3d50617648194178cd26ebcc21919d2cea6753
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-17 17:45:22 -0400

    t

--- db/migrate/20240606161407_create_vcenter_credentials.rb
+++ db/migrate/20240606161407_create_vcenter_credentials.rb
@@ -4,9 +4,10 @@ class CreateVcenterCredentials < ActiveRecord::Migration[7.1]
       t.string :vsphere_server
       t.string :username
       t.string :password
+      t.integer :vcenter_id
       t.boolean :ssl_verification
-
       t.timestamps
     end
+    add_index :vcenter_credentials, :vcenter_id
   end
 end
