0645252942c7ba79059e1986f3dd7934b427c8c4
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-17 11:15:31 -0400

    t

--- app/controllers/subnets_controller.rb
+++ app/controllers/subnets_controller.rb
@@ -1,36 +1,48 @@
 class SubnetsController < ApplicationController
-    before_action :set_vm_networks, only: [:new, :create]
-  
-    def new
-      @subnet = Subnet.new
-    end
-  
-    def create
-      @subnet = Subnet.new(subnet_params)
-      if @subnet.save
-        redirect_to subnets_path, notice: 'Subnet was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def index
-      @subnets = Subnet.all
-    end
-  
-    private
-  
-    def subnet_params
-      params.require(:subnet).permit(:name, :address, :cidr, :mask, :gateway, :dns1, :dns2, :vm_network)
-    end
-  
-    def set_vm_networks
-      @vm_networks = fetch_vm_networks
-    end
-  
-    def fetch_vm_networks
-      # Assuming you have a method to fetch VM networks from vCenter
-      Vcenter.all.map(&:vm_networks).flatten
+  before_action :set_subnet, only: [:edit, :update, :destroy]
+
+  def index
+    @subnets = Subnet.all
+  end
+
+  def new
+    @subnet = Subnet.new
+    @vm_networks = VmNetwork.all # Assuming you have a VmNetwork model
+  end
+
+  def create
+    @subnet = Subnet.new(subnet_params)
+    if @subnet.save
+      redirect_to subnets_path, notice: 'Subnet was successfully created.'
+    else
+      render :new
     end
   end
-  
+
+  def edit
+    @vm_networks = VmNetwork.all
+  end
+
+  def update
+    if @subnet.update(subnet_params)
+      redirect_to subnets_path, notice: 'Subnet was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @subnet.destroy
+    redirect_to subnets_path, notice: 'Subnet was successfully deleted.'
+  end
+
+  private
+
+  def set_subnet
+    @subnet = Subnet.find(params[:id])
+  end
+
+  def subnet_params
+    params.require(:subnet).permit(:name, :address, :cidr, :mask, :gateway, :dns1, :dns2, :vm_network_id)
+  end
+end
