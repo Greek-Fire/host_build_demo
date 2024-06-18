7261f145316e610df4c09e87acc3aab253402150
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-18 18:05:43 -0400

    t

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # config/importmap.rb
 pin "application", to: "application.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/ujs", to: "https://cdn.skypack.dev/@rails/ujs@6"
