9ba70654c4f14149e3dfe48158c56033536e79b6
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-18 18:09:18 -0400

    t

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// app/javascript/packs/application.js
+import Rails from "@rails/ujs";
+Rails.start();
