2a47dbb06f29b1a76f647ca57d467420f0067385
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-18 17:56:06 -0400

    t

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
-const { environment } = require('@rails/webpacker')
+// In config/webpack/environment.js
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+// Example of setting an entry point explicitly
+environment.entry = {
+  application: './app/javascript/packs/application.js'
+};
+
+module.exports = environment;
