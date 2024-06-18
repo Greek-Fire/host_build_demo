01d21e7c776a0a56ed318ccadd5c75f25ea9a00e
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-18 09:54:21 -0400

    t

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 # config/routes.rb
 Rails.application.routes.draw do
   resources :vcenters do
-    member do
-      post :update_datacenters
+    resources :vcenter_credentials, only: [:new, :create, :edit, :update, :destroy] do
+      post 'verify', on: :member
     end
-    resources :datacenters do
-      resources :compute_clusters do
-        resources :datastores, only: [:show]
-        resources :networks, only: [:show]
+    member do
+      post 'update_datacenters'
+    end
+    resources :datacenters, only: [:show] do
+      resources :compute_clusters, only: [:show] do
+        get 'networks', on: :member
       end
     end
   end
-  resources :subnets
-  root "home#index"
+
+  root to: 'home#index'
 end
