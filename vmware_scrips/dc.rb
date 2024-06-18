d56f80e336ff9ea4827ccb00ce0a6d1c3dbf83fa
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-18 15:04:09 -0400

    t

--- vmware_scrips/dc.rb
+++ vmware_scrips/dc.rb
@@ -13,12 +13,28 @@ password = 'Gr33k*G0d7'
 client = VMwareClient.new(host: vcenter_host, user: username, password: password)
 
 # Collect and print detailed information about datacenters
+# dc.rb
+
+require_relative 'VMwareClient'
+
+# Replace with your vSphere connection details
+vcenter_host = 'your-vcenter-hostname'
+username = 'your-username'
+password = 'your-password'
+
+# Instantiate VMwareClient with connection details
+client = VMwareClient.new(host: vcenter_host, user: username, password: password)
+
+# Collect and print detailed information about datacenters with structured format
 begin
   datacenters = client.collect_datacenters
   datacenters.each do |dc|
     puts "Datacenter: #{dc[:name]}"
     dc[:datastore_clusters].each do |dsc|
       puts "  Datastore Cluster: #{dsc[:name]}, Total Storage: #{dsc[:total_storage]}"
+      dsc[:datastores].each do |ds|
+        puts "    Datastore: #{ds[:name]}, Capacity: #{ds[:capacity]}, Free Space: #{ds[:free_space]}"
+      end
     end
     dc[:compute_clusters].each do |cc|
       puts "  Compute Cluster: #{cc[:name]}"
@@ -27,14 +43,18 @@ begin
       end
       cc[:hosts].each do |host|
         puts "    Host: #{host[:name]}, IP: #{host[:ip_address]}"
+        host[:vm_networks].each do |net|
+          puts "      VM Network on Host: #{net}"
+        end
       end
     end
     dc[:datastores].each do |ds|
-      puts "  Datastore: #{ds[:name]}, Free Space: #{ds[:free_space]}, Capacity: #{ds[:capacity]}"
+      puts "  Standalone Datastore: #{ds[:name]}, Capacity: #{ds[:capacity]}, Free Space: #{ds[:free_space]}"
     end
   end
 rescue StandardError => e
   puts "Error: #{e.message}"
 ensure
+  # Ensure to disconnect from vSphere
   client.disconnect
 end
