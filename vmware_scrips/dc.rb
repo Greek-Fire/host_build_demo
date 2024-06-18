ed257d7f97c351ac3db24eff1a1a6bc867c0b5a0
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-06-18 13:29:05 -0400

    t

--- vmware_scrips/dc.rb
+++ vmware_scrips/dc.rb
@@ -8,16 +8,15 @@ vcenter_host = 'vcenter.lou.land'
 username = 'administrator@lou.land'
 password = 'Gr33k*G0d7'
 
-# Instantiate VMwareClient with connection details
 client = VMwareClient.new(host: vcenter_host, user: username, password: password)
 
 # Example: Collect and print datacenters
 begin
   datacenters = client.collect_datacenters
-  puts "Datacenters: #{datafaces.join(', ')}"
+  puts "Datacenters: #{datacenters.join(', ')}"
 rescue StandardError => e
   puts "Error: #{e.message}"
 ensure
-  # Ensure to disconnect from vSphere after operation
+  # Disconnect from vSphere
   client.disconnect
 end
